aae8de3df7010f9916ba0c8804f846cddf528bf0
Abdullah Alsalem <abusaud6@gmail.com>
2023-05-23 08:51:20 +0300

    البدء في صفحة الشفرات

--- src/app/pages/ciphers/ciphers.component.ts
+++ src/app/pages/ciphers/ciphers.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { CiphersService } from 'src/app/services/ciphers.service';
 
 @Component({
   selector: 'app-ciphers',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./ciphers.component.scss']
 })
 export class CiphersComponent {
+  items: any = [];
+
+  constructor(
+    private ciphersService: CiphersService,
+    public translateService: TranslateService
+  ) {
+    this.ciphersService.getItems().subscribe(res => {
+      this.items = res;
+    });
+  }
+
+
 
 }
