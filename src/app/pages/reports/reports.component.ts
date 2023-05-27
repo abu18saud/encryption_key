95de275ff375b3f71c5a3b62877ccb1ec535c69f
Abdullah Alsalem <441226478@tvtc.edu.sa>
2023-05-27 13:49:10 +0300

    الانتهاء من صفحة التقارير

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ReportsService } from 'src/app/services/reports.service';
 
 @Component({
   selector: 'app-reports',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./reports.component.scss']
 })
 export class ReportsComponent {
-
+  items: any = [];
+  constructor(private reportsService: ReportsService,
+    public translateService: TranslateService) {
+    this.reportsService.getItems().subscribe(res => {
+      this.items = res;
+    });
+  }
 }
