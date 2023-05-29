2e2a8c6b709f44538a4be3849b3585c70cd0bca2
Abdullah Alsalem <441226478@tvtc.edu.sa>
2023-05-29 20:01:05 +0300

    لإضافة أزرار وتجميلها لاستعراض ملفات الـ PDF

--- src/app/tools/report-dialog/report-dialog.component.ts
+++ src/app/tools/report-dialog/report-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-report-dialog',
@@ -14,4 +15,12 @@ export class ReportDialogComponent {
     @Inject(MAT_DIALOG_DATA) public data: any,
   ) { }
   pdfSrc = this.data.item.report_file;
+
+  ngOnInit() {
+    this.pdfSrc = this.data.item.report_file;
+  }
+
+  downloadFile() {
+    FileSaver.saveAs(this.pdfSrc, this.data.item.en_name + " - " + new Date().getDate() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear() + '.pdf');
+  }
 }
